fbc8730373eecb90e0598849d40fbf66713a5a4a
jasmineytchen <jasmineytchen@gmail.com>
2019-04-11 14:26:54 +0100

    name test

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -4,6 +4,8 @@ public class QueryProcessor {
 
     public String process(String query) {
 
+        System.out.println(query);
+
         if (query.toLowerCase().contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
@@ -12,6 +14,7 @@ public class QueryProcessor {
         if (query.toLowerCase().contains("name")) {
             return "mexico-bandido";
         }
+
 
         if (query.toLowerCase().contains("largest")) {
             System.out.println(query.split(":",2));
